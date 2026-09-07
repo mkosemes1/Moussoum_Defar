8115b30b996b4e5c04ee35ea153c6fe3adcdcfac
MK-OSEMES <malickoseme@gmail.com>
2026-09-07 19:43:25 +0000

    Add NoCacheTemplates middleware to force no-cache on all template pages

--- Moussoum_Defar/settings.py
+++ Moussoum_Defar/settings.py
@@ -38,6 +38,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'Moussoum_Defar.middleware.NoCacheTemplates',
 ]
 
 ROOT_URLCONF = 'Moussoum_Defar.urls'
